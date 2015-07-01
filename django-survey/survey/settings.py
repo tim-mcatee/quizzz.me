e9765cb2df9943598f8233be67e250a46e6e562d
Tim McAtee <tim.is.working@gmail.com>
2015-06-30 20:54:48 -0700

    2nd upload, mostly for testing purposes

--- django-survey/survey/settings.py
+++ django-survey/survey/settings.py
@@ -10,18 +10,18 @@ ROOT = os.path.dirname(os.path.abspath(__file__))
 path = lambda *a: os.path.join(ROOT, *a)
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+     ('Tim', 'tim.is.working@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'survey.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
+        'ENGINE': 'mysql.connector.django', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'django_survey',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
@@ -130,7 +130,7 @@ INSTALLED_APPS = (
     'survey',
 )
 
-support_email = "survey@coliving.org"
+support_email = "tim.is.working@gmail.com"
 
 
 # A sample logging configuration. The only tangible logging
