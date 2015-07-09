039542b708ece6d459078a99486ca17725c3f703
Tim McAtee <tim.is.working@gmail.com>
2015-07-09 12:57:58 -0700

    updated settings for deployment, but still not working

--- django-survey/survey/settings.py
+++ django-survey/survey/settings.py
@@ -1,14 +1,18 @@
-# Django settings for survey project.
+# Django settings for survey project. updated to django 1.8.2
 
 import os 
+import socket
 
-
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-# Make filepaths relative to settings.
-ROOT = os.path.dirname(os.path.abspath(__file__))
-path = lambda *a: os.path.join(ROOT, *a)
+if socket.gethostname() == 'Tims-MacBook-Air.local':
+    DEBUG = True
+    TEMPLATE_DEBUG = DEBUG
+    ROOT = os.path.dirname(os.path.abspath(__file__))
+    path = lambda *a: os.path.join(ROOT, *a)
+else:
+    DEBUG = False
+    TEMPLATE_DEBUG = False
+    # Make filepaths relative to settings.
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ADMINS = (
      ('Tim', 'tim.is.working@gmail.com'),
@@ -16,16 +20,45 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'mysql.connector.django', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django_survey',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+if socket.gethostname() == 'Tims-MacBook-Air.local':
+    ALLOWED_HOSTS = [
+        'localhost:8000',
+        'Tims-MacBook-Air.local',
+        '.local',
+    ]
+else:     
+    ALLOWED_HOSTS = [
+        '.webfaction.com',
+        '.quizzz.me',
+        'quizzz.me',
+        'www.quizzz.me',
+    ]
+
+if socket.gethostname() == 'Tims-MacBook-Air.local':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'mysql.connector.django', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'django_survey',                      # Or path to database file if using sqlite3.
+            'USER': 'root',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'mysql.connector.django', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'django_survey',                      # Or path to database file if using sqlite3.
+            'USER': 'ds_admin',                      # Not used with sqlite3.
+            'PASSWORD': '99Problemos',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+            # 'OPTIONS': {
+            #     'init_command': 'SET storage_engine=INNODB',
+            # },
+        }
+    }
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -88,23 +121,17 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'rzk0!e3eav79hs51!m4g3w!&amp;52&amp;o5e8yj)g70!f8$v^_vz)p7-'
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
+SECRET_KEY = 'rzk0!e2eAv79hs51!v4g2w!&amp;52&amp;o5e8yj)g70!f8$v^_vz)p7-'
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'survey.urls'
@@ -112,12 +139,6 @@ ROOT_URLCONF = 'survey.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'survey.wsgi.application'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    path("../templates/"),
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -131,34 +152,73 @@ INSTALLED_APPS = (
     'survey',
 )
 
-support_email = "tim.is.working@gmail.com"
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
 
+if socket.gethostname() == 'Tims-MacBook-Air.local':
+    TEMPLATE_DIRS = (
+        # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+        # Always use forward slashes, even on Windows.
+        # Don't forget to use absolute paths, not relative paths.
+        path("../templates/"),
+    )
+else:
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': ["/home/timcatee/webapps/quizapp/django-survey/templates" ],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
+
+
+# support_email = "tim.is.working@gmail.com"
+
+# EMAIL_HOST = 'smtp.webfaction.com'
+
+# EMAIL_HOST_USER = 'quizzzmebox'
+
+# EMAIL_HOST_PASSWORD = '99Problemos'
+
+# SERVER_EMAIL = 'tim@quizzz.me'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'filters': {
+#         'require_debug_false': {
+#             '()': 'django.utils.log.RequireDebugFalse'
+#         }
+#     },
+#     'handlers': {
+#         'mail_admins': {
+#             'level': 'ERROR',
+#             'filters': ['require_debug_false'],
+#             'class': 'django.utils.log.AdminEmailHandler'
+#         }
+#     },
+#     'loggers': {
+#         'django.request': {
+#             'handlers': ['mail_admins'],
+#             'level': 'ERROR',
+#             'propagate': True,
+#         },
+#     }
+# }
